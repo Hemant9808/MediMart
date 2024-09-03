bec29ca1c1464761b02d64cf5b3ecd4b42e66450
hemant9808 <hemant27134@gmail.com>
2024-09-03 16:33:56 +0530

    fix

--- client/src/Redux/authSlice/authSlice.js
+++ client/src/Redux/authSlice/authSlice.js
@@ -1,97 +1,106 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const LOGIN_API = 'https://medimart-nayg.onrender.com/login'
-const REGISTER_API = 'https://medimart-nayg.onrender.com/signup'
+const LOGIN_API = "https://medimart-nayg.onrender.com/login";
+const REGISTER_API = "http://localhost:4000/auth/signup";
+
+//const REGISTER_API = 'https://medimart-nayg.onrender.com/auth/signup'
 // const LOGOUT_API = 'logout'
 
 export const loginAccount = createAsyncThunk(
-    'auth/loginAccount',
-    async (credentials) => {
-        console.log('loginAccount credentials:-', credentials);
-        try {
-            const formData = new FormData();
+  "auth/loginAccount",
+  async (credentials) => {
+    console.log("loginAccount credentials:-", credentials);
+    try {
+      const formData = new FormData();
 
-            formData.append("email", credentials.email);
-            formData.append("password", credentials.password);
+      formData.append("email", credentials.email);
+      formData.append("password", credentials.password);
 
+      const response = await axios.post(LOGIN_API, formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("response- login !=", response);
 
-            const response = await axios.post(LOGIN_API,formData);
-            console.log('response- login !=',response);
-            
-            return response;
-        } catch (error) {
-            console.log('loginAccount error:- ', error);
-            return error.message
-        }
+      return response;
+    } catch (error) {
+      console.log("loginAccount error:- ", error);
+      return error.message;
     }
-  );
+  }
+);
 export const createAccount = createAsyncThunk(
-    'auth/createAccount',
-    async (credentials) => {
-        // console.log('createAccount credentials:-', credentials);
-        try {
-            const formData = new FormData();
+  "auth/createAccount",
+  async (credentials) => {
+    // console.log('createAccount credentials:-', credentials);
+    try {
+      const formData = new FormData();
 
-            formData.append("firstName", credentials.firstName);
-            formData.append("lastName", credentials.lastName);
-            formData.append("userName", credentials.userName);
-            formData.append("phone", credentials.phone);
-            formData.append("email", credentials.email);
-            formData.append("password", credentials.password);
-            formData.append("cmPassword", credentials.cmPassword);
+      formData.append("firstName", credentials.firstName);
+      formData.append("lastName", credentials.lastName);
+      formData.append("userName", credentials.userName);
+      formData.append("phone", credentials.phone);
+      formData.append("email", credentials.email);
+      formData.append("password", credentials.password);
+      formData.append("cmPassword", credentials.cmPassword);
 
-            const response = await axios.post(REGISTER_API,formData);
-            // console.log('response- login !=',response);
-            
-            return response;
-        } catch (error) {
-            console.log('createAccount error:- ', error);
-            return error.message
-        }
+      for (const [key, value] of formData.entries()) {
+        console.log(`${key}: ${value}`);
+      }
+      const response = await axios.post(REGISTER_API, formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      // console.log('response- login !=',response);
+
+      return response;
+    } catch (error) {
+      console.log("createAccount error:- ", error);
+      return error.message;
     }
-  );
-
-
+  }
+);
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState: {
     userDetails: "",
     loading: false,
-    error: null
+    error: null,
   },
-  reducers: {
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(loginAccount.pending, (state) => {
-        console.log('loginAccount.pending',  state);
+        console.log("loginAccount.pending", state);
         state.loading = true;
       })
       .addCase(loginAccount.fulfilled, (state, action) => {
-        console.log('loginAccount.fulfilled',  action);
+        console.log("loginAccount.fulfilled", action);
         state.loading = false;
         state.userDetails = action.payload;
       })
       .addCase(loginAccount.rejected, (state, action) => {
-        console.log('loginAccount.rejected',  action);
+        console.log("loginAccount.rejected", action);
         state.loading = false;
         state.error = action.error.message;
       });
     builder
       .addCase(createAccount.pending, (state) => {
-        console.log('createAccount.pending',  state);
+        console.log("createAccount.pending", state);
         state.loading = true;
       })
       .addCase(createAccount.fulfilled, (state, action) => {
-        console.log('createAccount.fulfilled',  action);
+        console.log("createAccount.fulfilled", action);
         state.loading = false;
-        localStorage.setItem("token", action.payload.token)
+        localStorage.setItem("token", action.payload.token);
         state.userDetails = action.payload;
       })
       .addCase(createAccount.rejected, (state, action) => {
-        console.log('createAccount.rejected',  action);
+        console.log("createAccount.rejected", action);
         state.loading = false;
         state.error = action.error.message;
       });
